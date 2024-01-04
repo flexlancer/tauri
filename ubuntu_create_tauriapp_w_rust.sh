8b21a4512252d2a448c074d653f87b561dcc7dc0
flexlancer-ch <62950511+flexlancer@users.noreply.github.com>
2024-01-04 13:16:47 -0600

    Add files via upload

---
+++ ubuntu_create_tauriapp_w_rust.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#Create Tauri app with rust
+
+sh <(curl https://create.tauri.app/sh)
+
+#https://tauri.app/v1/guides/getting-started/setup/
+
+cargo install tauri-cli
+
+# web assets:     ../ui 
+# url dev server: ../ui
+
+sudo apt install npm
+npm install --global yarn
+
